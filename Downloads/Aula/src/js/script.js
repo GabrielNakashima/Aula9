da2c802dc3202c357ecf0f3fe6729126b4222093
Naka <gabrielluni@gmail.com>
2024-05-02 11:06:18 -0300

    Criando função listar tarefas

--- Downloads/Aula/src/js/script.js
+++ Downloads/Aula/src/js/script.js
@@ -1,21 +1,20 @@
-//Declarando as var
-
+//Declarando as variaveis
 let tarefas=[];
 
 //Função que valida o preenchimento do campo caso esteja vazio
 
-function validaCampo(){
-    let valida = false;
-    if(document.getElementById("task").value =="")
-    valida = true;
+function validaCampo()
+{
+    let valida =false;
+    if(document.getElementById("task").value =="") 
+    valida =true;
     return valida;
 }
-
 //Função que adiciona a tarefa
 
 function adicionarTarefa(){
-        //Criando a variável que vai receber as tarefas
-    let linhas = document.getElementById("task");
+        //criando a variavel que vai receber as tarefas
+    let linhas =document.getElementById("task");
 
     if(validaCampo()){
         alert("Preencha o campo com a tarefa")
@@ -24,9 +23,19 @@ function adicionarTarefa(){
         tarefas.push(linhas.value);
         //passando para a proxima linha vazia
         linhas.value="";
-        //chamando a função que vai mostrar as tarefas
+        //chamando a função que vai mostrar todas as tarefas
         listarTarefas();
     }
     //volta para o input da tarefa
     document.getElementById("task").focus();
+}
+
+//Função Listar tarefas
+
+function listarTarefas(){
+    let valor="";
+    for(let i=0; i<tarefas.length;i++){
+        valor += tarefas[i] +"<br>";
+    }
+    document.getElementById("lista").innerHTML =valor;
 }
